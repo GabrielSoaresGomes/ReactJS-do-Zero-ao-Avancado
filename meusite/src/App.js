87c9817102179af38e4b3d0238478c0ce8c87652
Gabriel Soares gomes <dev.gabrielsoares221@gmail.com>
2022-04-10 10:48:08 -0300

    Utilizando mais de um component dentro de outro

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,10 +1,31 @@
 import React from 'react'
 
-const BemVindo = (props) => {
+const Equipe = (props) => {
     return (
         <div>
-            <h2>Bem-Vindo(a) {props.nome} </h2>
-            <h3>Tenho {props.idade} anos</h3>
+            <Sobre nome={props.nome} cargo={props.cargo} idade={props.idade} />
+            <Social fb={props.facebook} />
+            <hr/>
+        </div>
+    )
+}
+
+const Sobre = (props) => {
+    return (
+        <div>
+            <h2>Olá sou o(a) {props.nome}</h2>
+            <h3>Cargo: {props.cargo}</h3>
+            <h3>Idade: {props.idade} anos</h3>
+        </div>
+    )
+}
+
+const Social = (props) => {
+    return (
+        <div>
+            <a href={props.fb}> Facebook </a>
+            <a> Linkedin </a>
+            <a> Youtube </a>
         </div>
     )
 }
@@ -12,9 +33,10 @@ const BemVindo = (props) => {
 function App() {
     return (
         <div>
-            <p>Olá</p>
-            <BemVindo nome="Gabriel" idade={18} />
-            <BemVindo nome="Gabriel" idade={17} />
+            <h1>Conheça nossa equipe</h1>
+            <Equipe nome="Gabriel" cargo="Programador" idade="29" facebook="https://google.com" />
+            <Equipe nome="Pedro" cargo="Designer" idade="21" facebook="https://google.com" />
+            <Equipe nome="Amanda" cargo="Front-end" idade="24" facebook="https://google.com" />
         </div>
     )
 }
