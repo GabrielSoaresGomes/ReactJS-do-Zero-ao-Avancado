d045e3d7d92671ee81e32fa8da1f288f773ceedb
Gabriel Soares gomes <dev.gabrielsoares221@gmail.com>
2022-04-11 16:02:01 -0300

    Manipulando formulários

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,29 +1,55 @@
 import React, {Component} from 'react'
 
-import Feed from './components/feed'
-
 class App extends Component{
 
     constructor(props){
         super(props)
         this.state = {    
-            feed: [
-                {id: 1, username: "Matheus", curtidas:10, comentarios:2},
-                {id: 2, username: "Pedro", curtidas:120, comentarios: 24},
-                {id: 3, username: "Amanda", curtidas:167, comentarios: 33},
-                {id: 4, username: "André", curtidas:1, comentarios: 0},
-            ]
+            email: '',
+            senha: '',
+            sexo: 'masculino'
         }
+        this.trocaEmail = this.trocaEmail.bind(this)
+        this.mostrarDados = this.mostrarDados.bind(this)
+        this.trocarSexo = this.trocarSexo.bind(this)
+    }
+
+    trocaEmail(e) {
+        let valorDigitado = e.target.value;
+        this.setState({email: valorDigitado})
+    }
+
+    mostrarDados() {
+        document.getElementById('dados').style.display === 'block' ?
+        document.getElementById('dados').style.display = 'none':
+        document.getElementById('dados').style.display = 'block'
+    }
+    
+    trocarSexo(e) {
+        this.setState({sexo: e.target.value})
     }
 
     render() {
         return(
             <div>
-                {this.state.feed.map((item) => {
-                    return(
-                        <Feed id={item.id} username={item.username} curtidas={item.curtidas} comentarios={item.comentarios} />
-                    )
-                })}
+                <h2>Login</h2>
+                Email:
+                <input type="email" name="email" value={this.state.email} onChange={this.trocaEmail} /> <br/>
+                Senha:
+                <input id='inputSenha' type="password" name='senha' value={this.state.senha} onChange={(e) => this.setState({senha: e.target.value})} />
+                <button onClick={() => {
+                        document.getElementById('inputSenha').getAttribute('type') === "password" ? 
+                        document.getElementById('inputSenha').setAttribute('type', 'text') :
+                        document.getElementById('inputSenha').setAttribute('type', 'password')
+                    }} >Mostrar Senha</button>
+                <br/>
+                Sexo:
+                <select name="sexo" onChange={this.trocarSexo} value={this.state.sexo}>
+                    <option value='masculino'>Masculino</option>
+                    <option value="feminino">Feminino</option>
+                </select>
+                <button onClick={this.mostrarDados}>Mostrar Dados</button>
+                <p style={{display: 'none'}} id='dados'>EMAIL:{this.state.email} <br/> SENHA:{this.state.senha} <br/> SEXO:{this.state.sexo} </p>
             </div>
         )
     }
