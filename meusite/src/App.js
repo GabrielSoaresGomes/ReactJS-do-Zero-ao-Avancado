3391ddfd71947a70d3a14e2e89cc4aa87a62d84c
Gabriel Soares gomes <dev.gabrielsoares221@gmail.com>
2022-04-11 14:51:12 -0300

    Renderização Condicional

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,13 +1,38 @@
 import React, {Component} from 'react'
 
-import Membro from './components/Membro' //O react ja vai pegar o arquivo index dentro da pasta
-
 class App extends Component{
+
+    constructor(props){
+        super(props)
+        this.state = {
+            status: false
+        }
+        this.entrar = this.entrar.bind(this)
+        this.sair = this.sair.bind(this)
+    }
+
+    entrar() {
+        this.setState({status: true})
+    }
+
+    sair() {
+        this.setState({status: false})
+    }
 
     render() {
         return(
             <div>
-                <Membro nome={"Visitante"} />
+                {this.state.status ? 
+                <div>
+                    <h2>Bem-Vindo(a) ao Sistema!</h2>
+                    <button onClick={this.sair}>Sair!</button>
+                    
+                </div> ://Operador ternário JS
+                <div>
+                    <h2>Olá Visitante, faça o login!</h2>
+                    <button onClick={this.entrar}>Entrar no Sistema</button>
+                </div>
+            }
             </div>
         )
     }
