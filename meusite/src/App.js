a6725217a0d23d56be5f1dd1a49877e287f90bbd
Gabriel Soares gomes <dev.gabrielsoares221@gmail.com>
2022-04-11 17:21:52 -0300

    começando um form

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -2,54 +2,28 @@ import React, {Component} from 'react'
 
 class App extends Component{
 
-    constructor(props){
+    constructor(props) {
         super(props)
-        this.state = {    
-            email: '',
-            senha: '',
-            sexo: 'masculino'
+        this.state = {
+            nome:"",
+            email:"",
+            senha:"",
         }
-        this.trocaEmail = this.trocaEmail.bind(this)
-        this.mostrarDados = this.mostrarDados.bind(this)
-        this.trocarSexo = this.trocarSexo.bind(this)
+        this.mudarNome = this.mudarNome.bind(this)
     }
 
-    trocaEmail(e) {
-        let valorDigitado = e.target.value;
-        this.setState({email: valorDigitado})
-    }
-
-    mostrarDados() {
-        document.getElementById('dados').style.display === 'block' ?
-        document.getElementById('dados').style.display = 'none':
-        document.getElementById('dados').style.display = 'block'
-    }
-    
-    trocarSexo(e) {
-        this.setState({sexo: e.target.value})
+    mudarNome(e) {
+        this.setState({'nome':e.target.value})
     }
 
     render() {
         return(
             <div>
-                <h2>Login</h2>
-                Email:
-                <input type="email" name="email" value={this.state.email} onChange={this.trocaEmail} /> <br/>
-                Senha:
-                <input id='inputSenha' type="password" name='senha' value={this.state.senha} onChange={(e) => this.setState({senha: e.target.value})} />
-                <button onClick={() => {
-                        document.getElementById('inputSenha').getAttribute('type') === "password" ? 
-                        document.getElementById('inputSenha').setAttribute('type', 'text') :
-                        document.getElementById('inputSenha').setAttribute('type', 'password')
-                    }} >Mostrar Senha</button>
-                <br/>
-                Sexo:
-                <select name="sexo" onChange={this.trocarSexo} value={this.state.sexo}>
-                    <option value='masculino'>Masculino</option>
-                    <option value="feminino">Feminino</option>
-                </select>
-                <button onClick={this.mostrarDados}>Mostrar Dados</button>
-                <p style={{display: 'none'}} id='dados'>EMAIL:{this.state.email} <br/> SENHA:{this.state.senha} <br/> SEXO:{this.state.sexo} </p>
+               <h1>Novo Usuário</h1>
+               <form>
+                   <label for="nome">Nome: </label>
+                   <input type="text" name="nome" value={this.state.nome} onChange={this.mudarNome} />
+               </form>
             </div>
         )
     }
