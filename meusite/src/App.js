2401b0b8ff3dc0b7f19df2cc4be14a98632b5327
Gabriel Soares gomes <dev.gabrielsoares221@gmail.com>
2022-04-13 17:18:32 -0300

    Praticando um pouco mais de formulário!

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -8,8 +8,24 @@ class App extends Component{
             nome:"",
             email:"",
             senha:"",
+            error:"",
         }
         this.mudarNome = this.mudarNome.bind(this)
+        this.cadastrar = this.cadastrar.bind(this)
+    }
+
+    cadastrar(event) {
+        const {nome, email, senha} = this.state
+
+        if(nome !== '' && email !== '' && senha !== ""){
+            alert(`Nome: ${nome} \nEmail: ${email} \nSenha: ${senha}`)
+        }else {
+            this.setState({"error": "Opa, esqueceu de preencher algo!"})
+        }
+
+        
+
+        event.preventDefault()
     }
 
     mudarNome(e) {
@@ -20,9 +36,16 @@ class App extends Component{
         return(
             <div>
                <h1>Novo Usuário</h1>
-               <form>
+               {this.state.error && <p>{this.state.error}</p> }
+               <form onSubmit={this.cadastrar}>
                    <label for="nome">Nome: </label>
-                   <input type="text" name="nome" value={this.state.nome} onChange={this.mudarNome} />
+                   <input type="text" name="nome" value={this.state.nome} onChange={this.mudarNome} /> <br/>
+                   <label for="email">Email: </label>
+                   <input type="email" name='email' value={this.state.email} onChange={(e) => {this.setState({'email':e.target.value})}} /> <br/>
+                   <label for="senha">Senha: </label>
+                   <input type="password" name='senha' value={this.state.senha} onChange={(e) => {this.setState({'senha': e.target.value})}} /> <br/>
+                   <button type='submit'>Cadastrar</button>
+
                </form>
             </div>
         )
